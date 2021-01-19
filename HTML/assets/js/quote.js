b690f8df6d4228b393c047a3fd17801b326a4904
Cedric Motillon <cedricmotillon@outlook.com>
2021-01-19 14:05:28 -0500

    JS Update

--- HTML/assets/js/quote.js
+++ HTML/assets/js/quote.js
@@ -1,5 +1,3 @@
-var stories = 20;
-
 $(window).ready(function () {
     $("#building-type").on("change", buildingTypeChange);
 })
@@ -8,40 +6,43 @@ $(window).ready(function () {
 
 function buildingTypeChange() {
     console.log(this.value);
-    if (this.value == "Residential") {
+    if (this.value == "residential") {
         $("#select").addClass("d-none");
         $("#commercial").addClass("d-none");
         $("#corporate").addClass("d-none");
         $("#hybrid").addClass("d-none");
         $("#residential").removeClass("d-none");
         $(window).ready(function() {
-            $("#number-of-appartments").on("change", residentialAppartmentChange);
+            $("#number-of-appartments").on("change", AppartmentChange);
         })
         $(window).ready(function() {
             $("#number-of-floors").on("change", residentialFloorChange);
         })
     }
 
-    else if (this.value == "Commercial") {
+    else if (this.value == "commercial") {
         $("#select").addClass("d-none");
         $("#residential").addClass("d-none");
         $("#corporate").addClass("d-none");
         $("#hybrid").addClass("d-none");
         $("#commercial").removeClass("d-none");
         $(window).ready(function() {
-            $("#number-of-elevators").on("change", commercialElevatorChange);
+            $("#number-of-elevators").on("change", ElevatorChange);
         })
     }
 
-    else if (this.value == "Corporate") {
+    else if (this.value == "corporate") {
         $("#select").addClass("d-none");
         $("#residential").addClass("d-none");
         $("#commercial").addClass("d-none");
         $("#hybrid").addClass("d-none");
         $("#corporate").removeClass("d-none");
+        $(window).ready(function() {
+            $("#number-of-floors").on("change", corporateFloorChange);
+        })
     }
 
-    else if (this.value == "Hybrid") {
+    else if (this.value == "hybrid") {
         $("#select").addClass("d-none");
         $("#residential").addClass("d-none");
         $("#corporate").addClass("d-none");
@@ -58,31 +59,42 @@ function buildingTypeChange() {
     }
 }
 
-function commercialElevatorChange() {
+function ElevatorChange() {
     console.log(this.value);
-    document.getElementById("commercial-return").value = this.value;
+    $("#commercial-return").val(this.value);
 }
 
-function residentialAppartmentChange() {
+function AppartmentChange() {
     console.log(this.value);
-    document.getElementById("residential-return").value = Math.ceil(this.value / document.getElementById("number-of-floors").value / 6);
-    if (document.getElementById("residential-return").value == Infinity) {
-        document.getElementById("residential-return").value = 0;
+    $("#residential-return").val(Math.ceil(this.value / $("#number-of-floors").val() / 6));
+    if ($("#residential-return").val() == Infinity) {
+        $("#residential-return").val(0);
     }
-    if (document.getElementById("number-of-floors").value >= 20) {
+    else if ($("#number-of-floors").val() > 20) {
         document.getElementById("residential-return").value *= Math.floor(document.getElementById("number-of-floors").value/20) * 2;
     }
 }
 
-function residentialFloorChange() {
-    console.log(this.value);
-    document.getElementById("residential-return").value = Math.ceil(document.getElementById("number-of-appartments").value / this.value / 6);
-    if (document.getElementById("residential-return").value == Infinity) {
-        document.getElementById("residential-return").value = 0;
+function residentialFloorChange() { 
+    console.log(this.value)
+    $("#residential-return").val(Math.ceil($("#number-of-appartments").val() / this.value / 6));
+    if ($("#residential-return").val() == Infinity) {
+        $("#residential-return").val(0);
     }
-    if (document.getElementById("number-of-floors").value >= 20) {
+    if ($("#number-of-floors").val() > 20) {
         document.getElementById("residential-return").value *= Math.floor(document.getElementById("number-of-floors").value/20) * 2;
     }
+}
+
+function corporateFloorChange() {
+    console.log(this.value)
+    var totalOccupants = $("#maximum-occupants").val() * this.value + $("#number-of-basements");
+    var elevatorRequired = Math.ceil(totalOccupants/1000);
+    var columnRequired = Math.ceil(($("#number-of-floors") + $("#number-of-basements"))/20);
+    var elevatorColumn = Math.ceil(elevatorRequired/columnRequired);
+    var totalElevator = elevatorColumn * columnRequired;
+
+    $("#corporate-return").val(totalElevator);
 }
 
 function onSubmit() {
